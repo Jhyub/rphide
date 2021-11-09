8253327e81804abaf5bda6563b428e7bab67cf66
Janghyub Seo <jhyub06@gmail.com>
2021-11-09 16:22:43 +0900

    I decided to depend on another library for windows only.

--- src/systray.rs
+++ src/systray.rs
@@ -1,5 +1,12 @@
 use std::sync::{Arc, Mutex};
+#[cfg(target_os = "linux")]
 use tray_item::TrayItem;
+#[cfg(target_os = "windows")]
+use trayicon::*;
+#[cfg(target_os = "windows")]
+use core::mem::MaybeUninit;
+#[cfg(target_os = "windows")]
+use winapi::um::winuser;
 use crate::config::Config;
 
 #[cfg(target_os = "linux")]
@@ -16,7 +23,7 @@ pub fn init(config: Arc<Mutex<Config>>) {
         let mut config = config.lock().unwrap();
         *config = Config::load();
     }).unwrap();
-    tray.add_menu_item("Edit config", move|| {
+        tray.add_menu_item("Edit config", move|| {
         open::that(Config::config_path());
     }).unwrap();
     tray.add_menu_item("Exit", move|| {
@@ -28,24 +35,49 @@ pub fn init(config: Arc<Mutex<Config>>) {
 
 #[cfg(target_os = "windows")]
 pub fn init(config: Arc<Mutex<Config>>) {
-    let mut tray = TrayItem::new("rphide", "icon.rc").unwrap();
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    enum Events {
+        Reload,
+        Edit,
+        Exit,
+    }
 
-    tray.add_label(format!("rphide version {}", env!("CARGO_PKG_VERSION")).as_str()).unwrap();
-    tray.add_label("by Jhyub").unwrap();
+    let (s, r) = std::sync::mpsc::channel::<Events>();
+    let icon = include_bytes!("../assets/rphide.ico");
 
+    let mut tray = TrayIconBuilder::new()
+        .sender(s)
+        .icon_from_buffer(icon)
+        .tooltip("rphide")
+        .menu(
+            MenuBuilder::new()
+                .item("Reload config", Events::Reload)
+                .item("Edit config", Events::Edit)
+                .item("Exit", Events::Exit)
+        )
+        .build().unwrap();
 
-    tray.add_menu_item("Reload config", move|| {
-        let mut config = config.lock().unwrap();
-        *config = Config::load();
-    }).unwrap();
-    tray.add_menu_item("Edit config", move|| {
-        open::that(Config::config_path());
-    }).unwrap();
-    tray.add_menu_item("Exit", move|| {
-        std::process::exit(0);
+    std::thread::spawn(move || {
+        r.iter().for_each(|m| match m {
+            Events::Exit => { std::process::exit(0); },
+            Events::Edit => { open::that(Config::config_path()); },
+            Events::Reload => {
+                let mut config = config.lock().unwrap();
+                *config = Config::load();
+            }
+        })
     });
 
-    tray.set_icon("icon.rc").unwrap();
-
-    std::io::stdin().read_line(&mut String::new()).unwrap();
+    loop {
+        unsafe {
+            let mut msg = MaybeUninit::uninit();
+            let bret = winuser::GetMessageA(msg.as_mut_ptr(), 0 as _, 0, 0);
+            if(bret > 0) {
+                winuser::TranslateMessage(msg.as_ptr());
+                winuser::DispatchMessageA(msg.as_ptr());
+            } else {
+                break;
+            }
+        }
+    }
 }
